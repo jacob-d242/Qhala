1448d314e12e52b6b4b47da4fbda48e2e3248860
jacob-d242 <jacobmoracha@gmail.com>
2023-03-27 18:57:50 +0300

    adding supabse

--- src/supabase/init.js
+++ src/supabase/init.js
@@ -1,1 +1,7 @@
 // 7fqKPAN4rlJeQ95y
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VUE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VUE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
